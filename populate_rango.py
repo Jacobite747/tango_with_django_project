3051d42943b1953168b4b390088f910a6c00dd5f
Jack Moulson <jacobite747@gmail.com>
2018-01-24 19:13:07 +0000

    add chapter 6 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -9,27 +9,35 @@ def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url":"http://docs.python.org/2/tutorial/"},
+         "url":"http://docs.python.org/2/tutorial/",
+         "views": 10},
         {"title":"How to Think like a Computer Scientist",
-         "url":"http://www.greenteapress.com/thinkpython/"},
+         "url":"http://www.greenteapress.com/thinkpython/",
+         "views": 25},
         {"title":"Learn Python in 10 Minutes",
-         "url":"http://www.korokithakis.net/tutorials/python/"}
+         "url":"http://www.korokithakis.net/tutorials/python/",
+         "views": 64}
     ]
 
     django_pages = [
         {"title":"Official Django Tutorial",
-         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 5},
         {"title":"Django Rocks",
-         "url":"http://www.djangorocks.com/"},
+         "url":"http://www.djangorocks.com/",
+         "views": 82},
         {"title":"How to Tango with Django",
-         "url":"http://www.tangowithdjango.com/"}
+         "url":"http://www.tangowithdjango.com/",
+         "views": 71}
     ]
 
     other_pages = [
         {"title":"Bottle",
-         "url":"http://bottlepy.org/docs/dev/"},
+         "url":"http://bottlepy.org/docs/dev/",
+         "views": 3},
         {"title":"Flask",
-         "url":"http://flask.pocoo.org"} 
+         "url":"http://flask.pocoo.org",
+         "views": 7} 
     ]
 
     cats = {
@@ -41,8 +49,9 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
+    # For printing to console only
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
